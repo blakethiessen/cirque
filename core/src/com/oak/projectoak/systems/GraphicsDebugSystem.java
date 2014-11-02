518d686a5f5415ed1ba46ccdd504311ed6b0f092
blakethiessen <blake.w.thiessen@gmail.com>
2014-11-01 21:11:44 -0700

    Removed java timer from GraphicsDebugSystem and resized game window for HTML5.

--- core/src/com/oak/projectoak/systems/GraphicsDebugSystem.java
+++ core/src/com/oak/projectoak/systems/GraphicsDebugSystem.java
@@ -5,11 +5,10 @@ import com.badlogic.gdx.graphics.Camera;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.utils.Timer;
 import com.oak.projectoak.Constants;
 
 import java.util.ArrayList;
-import java.util.Timer;
-import java.util.TimerTask;
 
 public class GraphicsDebugSystem extends EntitySystem
 {
@@ -45,10 +44,12 @@ public class GraphicsDebugSystem extends EntitySystem
         }
     }
 
-    public static void drawDebugRectangle(Vector2 twoDPosition, float width, float height, float rotation, Color color, int drawTimeMs)
+    public static void drawDebugRectangle(Vector2 twoDPosition, float width, float height, float rotation,
+                                          Color color, float drawTimeSec)
     {
         debugDrawables.add(new DebugDrawable(twoDPosition.scl(Constants.METERS_TO_PIXELS),
-                Constants.ConvertMetersToPixels(width), Constants.ConvertMetersToPixels(height), (float)Math.toDegrees(rotation), color, drawTimeMs));
+                Constants.ConvertMetersToPixels(width), Constants.ConvertMetersToPixels(height),
+                (float)Math.toDegrees(rotation), color, drawTimeSec));
     }
 
     private static class DebugDrawable
@@ -59,7 +60,8 @@ public class GraphicsDebugSystem extends EntitySystem
         public final Color color;
         private final float rotation;
 
-        public DebugDrawable(Vector2 twoDPosition, float width, float height, float rotation, Color color, int drawTime)
+        public DebugDrawable(
+                Vector2 twoDPosition, float width, float height, float rotation, Color color, float drawTime)
         {
             this.twoDPosition = twoDPosition;
             this.width = width;
@@ -73,7 +75,7 @@ public class GraphicsDebugSystem extends EntitySystem
 
                 final DebugDrawable that = this;
 
-                timer.schedule(new TimerTask()
+                timer.schedule(new Timer.Task()
                 {
                     @Override
                     public void run()
